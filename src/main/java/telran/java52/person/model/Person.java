79e9235f7d9f99faaa57f6782376310f8e697c15
Larisa <Larisa@DESKTOP-9JK8JBF>
2024-06-09 19:13:32 +0300

    le

---
+++ src/main/java/telran/java52/person/model/Person.java
@@ -0,0 +1,31 @@
+package telran.java52.person.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+@Entity(name="Citizen") // это будет сущность таблицы
+@Table(name="person") // название таблицы с которой будут связяны сущности
+public class Person implements Serializable {
+	private static final long serialVersionUID = 2881753026638817581L;
+	@Id
+	Integer id;
+	@Setter
+	String name;
+	LocalDate birthDate;
+	@Setter
+	Address address;
+}
---
+++ src/main/java/telran/java52/person/service/PersonServiceImpl.java
@@ -0,0 +1,98 @@
+package telran.java52.person.service;
+
+import java.time.LocalDate;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import telran.java52.person.dao.PersonRepository;
+import telran.java52.person.dto.AddressDto;
+import telran.java52.person.dto.CityPopulationDto;
+import telran.java52.person.dto.PersonDto;
+import telran.java52.person.dto.exceptions.PersonNotFoundException;
+import telran.java52.person.model.Address;
+import telran.java52.person.model.Person;
+
+@Service
+@RequiredArgsConstructor
+public class PersonServiceImpl implements PersonService {
+	final PersonRepository personRepository;
+	final ModelMapper modelMapper;
+
+	@Transactional
+	@Override
+	public Boolean addPerson(PersonDto personDto) {
+		if (personRepository.existsById(personDto.getId())) {
+			return false;
+		}
+		personRepository.save(modelMapper.map(personDto, Person.class));
+		return true;
+	}
+
+	@Override
+	public PersonDto findPersonById(Integer id) {
+		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
+		return modelMapper.map(person, PersonDto.class);
+	}
+
+	@Transactional
+	@Override
+	public PersonDto removePerson(Integer id) {
+		Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException());
+		personRepository.delete(person);
+		return modelMapper.map(person, PersonDto.class);
+	}
+
+	@Transactional
+	@Override
+	public PersonDto updatePersonName(Integer id, String name) {
+		Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException());
+		person.setName(name);
+		personRepository.save(person);
+		return modelMapper.map(person, PersonDto.class);
+	}
+
+	@Transactional
+	@Override
+	public PersonDto updatePersonAddress(Integer id, AddressDto addressDto) {
+		Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException());
+		person.setAddress(modelMapper.map(addressDto, Address.class));
+		personRepository.save(person);
+		return modelMapper.map(person, PersonDto.class);
+	}
+
+	@Override
+	public PersonDto[] findPersonsByCity(String city) {
+		return personRepository.findPersonsByAddressCityIgnoreCase(city)
+				
+				.map(p -> modelMapper.map(p, PersonDto.class))
+				.toArray(PersonDto[]::new);
+	}
+
+	@Override
+	public PersonDto[] findPersonsByName(String name) {
+		return personRepository.findPersonsByNameIgnoreCase(name)
+				
+				.map(p -> modelMapper.map(p, PersonDto.class))
+				.toArray(PersonDto[]::new);
+	}
+
+	@Override
+	public PersonDto[] findPersonsBetweenAge(Integer minAge, Integer maxAge) {
+		LocalDate from = LocalDate.now().minusYears(maxAge);
+		LocalDate to = LocalDate.now().minusYears(minAge);
+		return personRepository.findPersonsByBirthDateBetween(from, to)
+			
+				.map(p -> modelMapper.map(p, PersonDto.class))
+				.toArray(PersonDto[]::new);
+	}
+
+	@Override
+	public Iterable<CityPopulationDto> getCitiesPopulation() {
+		
+		return personRepository.getCitiesPopulation();
+	}
+
+}
